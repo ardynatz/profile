7d1906564f199e5d018552507d0da0148037f68c
Ardy Pranata <ardiprana90@gmail.com>
2024-04-05 00:17:17 +0700

    style: mobile padding

--- src/components/card-hover-effect.tsx
+++ src/components/card-hover-effect.tsx
@@ -82,7 +82,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full md:p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-zinc-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-zinc-700 relative z-20",
         className
       )}
     >
